47aed3d796c369952a7d8287b26fd25bd5a8998e
OsmanCanCetlenbik <62367767+OsmanCanCetlenbik@users.noreply.github.com>
2025-10-04 11:52:37 +0300

    Delete app directory

--- app/src/types/navigation.ts
+++
@@ -1,9 +0,0 @@
-export type RootStackParamList = {
-    Auth: undefined; // kullanıcı giriş ekranı
-    Upload: undefined;
-    Style: { imageUri: string };
-    Results: { imageUri: string; preset: string; resultUrl?: string };
-    Paywall: undefined;
-    History: undefined;
-  };
-  
